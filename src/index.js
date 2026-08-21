2720dfcead7baee660fc786f52c8062681336226
davidwgrey14-max <davidwgrey14@gmail.com>
2026-08-21 21:02:37 +0300

    Fix: Define 'ua' variable and fix IconButton in ManagerLogin

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ErrorDisplay from './components/ErrorDisplay';
 
 // Create root
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render with error handling
+// Render App
 root.render(
   <React.StrictMode>
-    <ErrorDisplay />
     <App />
   </React.StrictMode>
 );
